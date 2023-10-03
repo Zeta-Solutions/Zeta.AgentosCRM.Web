56b783c3ac762119a39835483f86a80244396d68
haider.shan1 <haider.shan@zetasolutionsonline.com>
2023-10-03 15:01:33 +0500

    Category,DegreeLevel,IeltsTranning,PreferStudyArea,Subjecct,Priority,Qualification,SubjectArea Added UI First Commit

---
+++ src/Zeta.AgentosCRM.Web.Mvc/Areas/AppAreaName/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zeta.AgentosCRM.Web.Controllers;
+using Zeta.AgentosCRM.Authorization;
+using Zeta.AgentosCRM.CRMSetup;
+using Zeta.AgentosCRM.CRMSetup.Dtos;
+using Abp.Application.Services.Dto;
+using Abp.Extensions;
+using Zeta.AgentosCRM.Web.Areas.AppAreaName.Models.MasterCategories;
+using System.Threading.Tasks;
+
+namespace Zeta.AgentosCRM.Web.Areas.AppAreaName.Controllers
+{
+    [Area("AppAreaName")]
+    public class CategoryController : AgentosCRMControllerBase
+    { 
+        public IActionResult Index()
+        {  
+            return View();
+           // return View();
+        }
+        public async Task<PartialViewResult> ViewCategoryModal(int id)
+        { 
+            return PartialView("_ViewCategoryModal", "");
+        }
+        public async Task<PartialViewResult> CreateOrEditModal(int? id)
+        {
+            return PartialView("_CreateOrEditModal", "");
+        }
+     
+    }
+}
---
+++ src/Zeta.AgentosCRM.Web.Mvc/wwwroot/view-resources/Areas/AppAreaName/Views/Subject/_CreateOrEditModal.js
@@ -0,0 +1,71 @@
+﻿(function ($) {
+  app.modals.CreateOrEditPartnerTypeModal = function () {
+    var _partnerTypesService = abp.services.app.partnerTypes;
+
+    var _modalManager;
+    var _$partnerTypeInformationForm = null;
+
+    var _PartnerTypemasterCategoryLookupTableModal = new app.ModalManager({
+      viewUrl: abp.appPath + 'AppAreaName/PartnerTypes/MasterCategoryLookupTableModal',
+      scriptUrl:
+        abp.appPath +
+        'view-resources/Areas/AppAreaName/Views/PartnerTypes/_PartnerTypeMasterCategoryLookupTableModal.js',
+      modalClass: 'MasterCategoryLookupTableModal',
+    });
+
+    this.init = function (modalManager) {
+      _modalManager = modalManager;
+
+      var modal = _modalManager.getModal();
+      modal.find('.date-picker').daterangepicker({
+        singleDatePicker: true,
+        locale: abp.localization.currentLanguage.name,
+        format: 'L',
+      });
+
+      _$partnerTypeInformationForm = _modalManager.getModal().find('form[name=InformationsForm]');
+      _$partnerTypeInformationForm.validate();
+    };
+
+    $('#OpenMasterCategoryLookupTableButton').click(function () {
+      var partnerType = _$partnerTypeInformationForm.serializeFormToObject();
+
+      _PartnerTypemasterCategoryLookupTableModal.open(
+        { id: partnerType.masterCategoryId, displayName: partnerType.masterCategoryName },
+        function (data) {
+          _$partnerTypeInformationForm.find('input[name=masterCategoryName]').val(data.displayName);
+          _$partnerTypeInformationForm.find('input[name=masterCategoryId]').val(data.id);
+        }
+      );
+    });
+
+    $('#ClearMasterCategoryNameButton').click(function () {
+      _$partnerTypeInformationForm.find('input[name=masterCategoryName]').val('');
+      _$partnerTypeInformationForm.find('input[name=masterCategoryId]').val('');
+    });
+
+    this.save = function () {
+      if (!_$partnerTypeInformationForm.valid()) {
+        return;
+      }
+      if ($('#PartnerType_MasterCategoryId').prop('required') && $('#PartnerType_MasterCategoryId').val() == '') {
+        abp.message.error(app.localize('{0}IsRequired', app.localize('MasterCategory')));
+        return;
+      }
+
+      var partnerType = _$partnerTypeInformationForm.serializeFormToObject();
+
+      _modalManager.setBusy(true);
+      _partnerTypesService
+        .createOrEdit(partnerType)
+        .done(function () {
+          abp.notify.info(app.localize('SavedSuccessfully'));
+          _modalManager.close();
+          abp.event.trigger('app.createOrEditPartnerTypeModalSaved');
+        })
+        .always(function () {
+          _modalManager.setBusy(false);
+        });
+    };
+  };
+})(jQuery);
